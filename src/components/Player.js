66ff116ebfc6212ef77a6e45eb2427b87e25eada
paolajfcw <67068066+paolajfcw@users.noreply.github.com>
2021-01-05 16:44:20 +0100

    Created a new music player project

---
+++ src/components/Player.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlayCircle,
+  faBackward,
+  faForward,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = () => {
+  return (
+    <div className="Player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+      <div className="play-control">
+        <FontAwesomeIcon className="backward" size="2x" icon={faBackward} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlayCircle} />
+        <FontAwesomeIcon className="forward" size="2x" icon={faForward} />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
---
+++ src/components/Song.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Song.scss";
+
+const Song = () => {
+  return (
+    <div className="Song">
+      <h1>Picture</h1>
+      <h1>Song Name</h1>
+      <h1>Artist</h1>
+    </div>
+  );
+};
+
+export default Song;
