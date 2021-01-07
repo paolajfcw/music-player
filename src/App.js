6a54a71a2d10cc7f406743a4bc0e02e2394ca3fd
paolajfcw <67068066+paolajfcw@users.noreply.github.com>
2021-01-07 19:48:08 +0100

    Added the data component and access to it from the app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React, { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Data from "./Data";
 
 import "./styles/App.scss";
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(Data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
